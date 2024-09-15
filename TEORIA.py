de5340655a03fafd466660ba7f8441048e06dae3
ALP300 <aitorpalomino1@gmail.com>
2024-09-14 20:09:49 -0500

    teoría de variables 1

---
+++ TEORIA.py
@@ -0,0 +1,22 @@
+#VARIABLES
+#STRING 
+mensaje= "Hola a todos"
+nombre= input("Por favor, ingresa tu nombre: ")
+correo= input("Por favor, ingresa tu correo: ")
+print("El mensaje es: ", mensaje)
+print("El nombre es: ", nombre)
+print("El correo es: ", correo)
+
+#INTEGER
+num1= int(input("Ingresa un número 1: "))
+num2= int(input("Ingresa un número 2: "))
+suma= num1+num2
+print("La suma es: ", suma)
+
+#FLOAT
+pi= 3.1416
+numero1= float(input("Ingresa un número 1: "))  
+numero2= float(input("Ingresa un número 2: "))
+suma2= numero1+numero2
+print("La suma es: ",suma2)
+print("El valor de pi es: ", pi)
---
+++ ejercicio4.py
@@ -0,0 +1,3 @@
+# Escribe un programa que solicite al usuario dos números y muestre su
+# suma, resta, multiplicación, división, división entera y residuo (módulo).
+
